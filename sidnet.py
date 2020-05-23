58cf31bdc99131edaca16f59a295278327a7b1c3
naughtyStark <sidharthtalia@ymail.com>
2020-05-23 13:13:48 +0530

    Update sidnet.py

--- sidnet.py
+++ sidnet.py
@@ -41,3 +41,45 @@ def alexnet(width,height,lr):  # Building 'AlexNet'
                         max_checkpoints=1, tensorboard_verbose=2,tensorboard_dir = 'log')
     return model
     
+def MILF(HEIGHT,WIDTH,lr):
+    # Pool1
+    network = input_data(shape=[None, HEIGHT, WIDTH, 1])
+    network_1 = conv_2d(network, 16, 3, activation='relu') #output 2x_downsampled
+    network_1 = conv_2d(network_1, 16, 3, activation='relu') #output 2x_downsampled
+    pool1 = max_pool_2d(network_1, 2)
+    #Pool2
+    network_2 = conv_2d(pool1, 32, 3, activation='relu') #output 4x_downsampled
+    network_2 = conv_2d(network_2, 32, 3, activation='relu') #output 4x_downsampled
+    pool2 = max_pool_2d(network_2, 2)
+    #Pool3
+    network_3 = conv_2d(pool2, 64, 3, activation='relu') #output 8x_downsampled
+    network_3 = conv_2d(network_3, 64, 3, activation='relu') #output 8x_downsampled
+    pool3 = max_pool_2d(network_3, 2)
+
+    #Pool4
+    network_3 = conv_2d(pool3, 128, 3, activation='relu') #output 16x_downsampled
+    network_3 = conv_2d(network_3, 128, 3, activation='relu') #output 16x_downsampled
+    pool4 = max_pool_2d(network_3, 2)
+
+    # ----- decoder ----- 
+    decoder = conv_2d_transpose(pool4, 128, 3, strides=4, output_shape=[HEIGHT//4, WIDTH//4, 128]) #  16x downsample to 4x downsample
+    decoder = conv_2d(decoder, 128, 3, activation='relu')
+    pool5 = conv_2d(decoder, 128, 3, activation='relu')
+ 
+    decoder = conv_2d_transpose(pool3, 64, 3, strides=2, output_shape=[HEIGHT//2, WIDTH//2, 64]) # 8x downsample to 4x downsample
+    decoder = conv_2d(decoder, 64, 3, activation='relu')
+    pool6 = conv_2d(decoder, 64, 3, activation='relu')
+
+    pool6=merge([pool6, pool5, pool2], mode='concat', axis=3) #merge all 4x downsampled layers
+
+    decoder = conv_2d_transpose(pool6, 32, 3, strides=4, output_shape=[HEIGHT, WIDTH, 32])
+    decoder = conv_2d(decoder, 32, 3, activation='relu')
+    pool6 = conv_2d(decoder, 32, 3, activation='relu')
+    CHANNELS = 1
+    decoder = conv_2d(pool6, CHANNELS, 1) # 3=CHANNELS
+    network = tflearn.regression(decoder, optimizer='adam', loss='mean_square',learning_rate=lr)
+    tflearn.init_graph(num_cores=4, gpu_memory_fraction=0.02)
+    model = tflearn.DNN(network, checkpoint_path='MILF_network',
+                        max_checkpoints=1, tensorboard_verbose=2,tensorboard_dir = 'log')
+
+    return model
